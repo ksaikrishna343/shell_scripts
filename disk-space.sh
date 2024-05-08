bcf3b7552582addb702a1ab18a9033ae220f22b6
Saikrishna <ksaikrishna343@gmail.com>
2024-05-08 08:34:59 -0500

    Create disk-space.sh
    Filesystem usage alert

---
+++ disk-space.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script help us to get notified if filesystems usage crosses threshold limit
+MAX=95
+EMAIL=server@127.0.0.1
+PART=sda1
+
+USE=$(df -h | grep $PART | awk '{ print $5 }' | cut -d'%' -f1)
+if [ $USE -gt $MAX ]; then
+	echo "Percent used: $USE" | mail -s "Running out of disk space" $EMAIL
+fi
